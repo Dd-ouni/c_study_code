1d6add78617e4afede60634c6f7d7269dc8e0570
东东 <34804413+Dd-ouni@users.noreply.github.com>
2020-11-10 08:38:44 +0800

    up 静态库 use

---
+++ 静态库/gcc使用/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "StaticLib1.h"
+
+int main(void)
+{
+  printf("add(10, 20) = %d \n", add(10, 20));
+  return 0;
+}
---
+++ 静态库/使用/使用/main.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include "StaticLib1.h"
+int main(int argc, char* argv[]) {
+	printf("%d \n", add(10, 20));
+	system("pause");
+	return 0;
+}
