3c1eeb2ab13b6be77b67715c6051c099b4e317ad
东东 <34804413+Dd-ouni@users.noreply.github.com>
2020-11-17 01:28:42 +0800

    up 汇编

---
+++ 机器码/gcc 配置/main.c
@@ -0,0 +1,8 @@
+int main(int argc, char const *argv[])
+{
+
+  // E:\software\Qt5.14.2\Tools\mingw730_64\bin\gcc.exe
+  // gcc -m64 -Og -S filename.c  ATT汇编格式代码 gcc默认格式
+  // gcc -m64 -Og -S -masm=intel filename.c  Intel格式出此之外都是这个格式
+  return 0;
+}
